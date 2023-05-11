422b5581386ccebce08294d71442073bc07922bc
Will <slorkda@gmail.com>
2023-05-11 16:14:46 -0700

    Worked on genfinal.sh

--- genfinal.sh
+++ genfinal.sh
@@ -2,3 +2,7 @@
 
 # Script for generating a .jar file that can be used on any system, as well as a .txt file that
 # contains all of the java code for this project (school project)
+
+FILES=$(ls | grep .java)
+
+tail $FILES --lines=+1 > final.txt
